cdfa54e7e12c501b8f0c63bb4c95ffcca321f36f
Stefan Marr <git@stefan-marr.de>
2018-04-24 13:24:19 +0100

    Simplify guards for array instantiation
    Previously, we checked various flags, which is more flexible since it allows complex subclassing.
    
    Now the guards are simplified to check for the known class. This should be ok for now, but will break if we subclass further.
    
    In that case, we should try a caching approach to keep a simple pointer comparison for the check.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/arrays/NewPrim.java
+++ src/som/primitives/arrays/NewPrim.java
@@ -47,19 +47,26 @@ public abstract class NewPrim extends BinaryExpressionNode {
     return receiver == Classes.arrayClass;
   }
 
-  @Specialization(guards = {"receiver.isArray()", "!receiver.isTransferObject()",
-      "!receiver.declaredAsValue()"})
+  protected static final boolean receiverIsValueArrayClass(final SClass receiver) {
+    return receiver == Classes.valueArrayClass;
+  }
+
+  protected static final boolean receiverIsTransferArrayClass(final SClass receiver) {
+    return receiver == Classes.transferArrayClass;
+  }
+
+  @Specialization(guards = "receiverIsArrayClass(receiver)")
   public static final SMutableArray createArray(final SClass receiver, final long length) {
     return new SMutableArray(length, receiver);
   }
 
-  @Specialization(guards = {"receiver.isArray()", "receiver.declaredAsValue()"})
+  @Specialization(guards = "receiverIsValueArrayClass(receiver)")
   public static final SImmutableArray createValueArray(final SClass receiver,
       final long length) {
     return new SImmutableArray(length, receiver);
   }
 
-  @Specialization(guards = {"receiver.isArray()", "receiver.isTransferObject()"})
+  @Specialization(guards = "receiverIsTransferArrayClass(receiver)")
   protected static final STransferArray createTransferArray(
       final SClass receiver, final long length) {
     return new STransferArray(length, receiver);
