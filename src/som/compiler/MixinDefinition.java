50110cb9704bb1a1c21bd1ba587c7da7bc6dd05d
clementbera <bera.clement@gmail.com>
2018-08-08 12:42:13 +0200

    fixed dynamic metric: (1) removed null hack (2) do not profile shadowStackEntry argument

--- src/som/compiler/MixinDefinition.java
+++ src/som/compiler/MixinDefinition.java
@@ -500,7 +500,7 @@ public final class MixinDefinition {
     VM.callerNeedsToBeOptimized(
         "only meant for code loading, which is supposed to be on the slowpath");
     CallTarget callTarget = superclassMixinResolution.createCallTarget();
-    SClass superClass = (SClass) callTarget.call(Nil.nilObject, null);
+    SClass superClass = (SClass) callTarget.call(Nil.nilObject);
     SClass classObject = instantiateClass(Nil.nilObject, superClass);
     return classObject;
   }
